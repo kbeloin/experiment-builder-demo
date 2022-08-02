4fd11807646de2b90d5c3cc4da9c7f44527d687c
kbeloin <kyle.beloin@me.com>
2022-08-01 19:51:29 -0700

    Added default field to model.

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    is_participant = models.BooleanField()
-    is_moderator = models.BooleanField()
+    is_participant = models.BooleanField(null=True, default=True)
+    is_moderator = models.BooleanField(null=True, default=False)
 
     def __str__(self):
         return self.username
